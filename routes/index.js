7eee991f8999fb85ffeedc3489216da9611a1c28
Michael Chen <tonghaoch@qq.com>
2019-12-10 17:37:46 +0800

    JSAEM2-9 feat: reply 404 for all unmatched requests

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,15 @@ const { redisClient } = require('../redisDB');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res) => {
-  res.end('OK');
-});
-
 router.get('/heartbeat', (req, res) => {
   if (redisClient.connected) {
     return res.sendStatus(200);
   }
   return res.sendStatus(500);
+});
+
+router.get('*', (req, res) => {
+  res.sendStatus(404);
 });
 
 module.exports = router;
