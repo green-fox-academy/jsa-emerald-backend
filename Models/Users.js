80be09ee75785a0a3f31042f8cf0bd64df068a8b
Michael Chen <tonghaoch@qq.com>
2019-12-25 17:01:38 +0800

    JSAEM2-31 signup backend (#11)
    * JSAEM2-31 signup backend
    
    * JSAEM2-31 lint issue
    
    * JSAEM 2-31 return error message

--- Models/Users.js
+++ Models/Users.js
@@ -3,6 +3,7 @@ const { mongoose } = require('../mongoDB');
 const usersBasic = new mongoose.Schema({
   username: String,
   email: String,
+  hashedPass: String,
 });
 
 const usersFull = new mongoose.Schema({
