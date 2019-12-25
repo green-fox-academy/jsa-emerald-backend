6a953923837720446b98ef8b06628f26e9f969df
Michael Chen <tonghaoch@qq.com>
2019-12-25 16:16:10 +0800

    JSAEM2-31 signup backend

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const debug = require('debug')('Emerald:Users');
 const { mongoose } = require('../mongoDB');
 const { usersBasic } = require('../Models/Users');
 const { verifyToken } = require('../Utils/Auth');
@@ -17,6 +19,7 @@ router.get('/', verifyToken, (req, res) => {
   const Users = mongoose.model('Users', usersBasic);
   Users.find((err, userList) => {
     if (err) {
+      debug(err);
       return res.sendStatus(500);
     }
 
@@ -26,6 +29,33 @@ router.get('/', verifyToken, (req, res) => {
 
     const filteredUserList = userList.filter((user) => user.username.includes(prefix));
     return res.json({ userList: filteredUserList });
+  });
+});
+
+router.post('/signup', (req, res) => {
+  const { username, email, password } = req.body;
+  const Users = mongoose.model('Users', usersBasic);
+  Users.find({ username, email }, (err, found) => {
+    if (err) {
+      debug(err);
+      return res.sendStatus(500);
+    }
+
+    if (found.length > 0) {
+      return res.sendStatus(400);
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPass = bcrypt.hashSync(password, salt);
+    const newUser = new Users({ username, email, hashedPass });
+    const token = jwt.sign({ username, email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    newUser.save((error) => {
+      if (error) {
+        debug(error);
+        return res.sendStatus(500);
+      }
+      return res.json({ authData: token });
+    });
   });
 });
 
