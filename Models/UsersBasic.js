6400eccbaefa47fd7f82156d3951dee495340836
Michael Chen <tonghaoch@qq.com>
2020-01-02 10:42:38 +0800

    JSAEM2-42 fixed: removed the useless file

--- Models/UsersBasic.js
+++
@@ -1,9 +0,0 @@
-const { mongoose } = require('../mongoDB');
-
-const usersBasic = new mongoose.Schema({
-  username: String,
-  email: String,
-  hashedPass: String,
-});
-
-module.exports = mongoose.model('users', usersBasic);
