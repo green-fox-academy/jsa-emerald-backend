83e8a1fcc5a8c7cfebcef9e1715338f4f2ea1716
Michael Chen <tonghaoch@qq.com>
2020-01-09 11:53:58 +0800

    JSAEM 2-46 fixed: use connection from initializer

--- mongoDB.test.js
+++ mongoDB.test.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
 const mongooseInit = require('./mongoDB');
 require('dotenv').config();
 
 describe('mongoDB', () => {
   it('Mongo DB Connection', async () => {
-    await mongooseInit();
-    expect(mongoose.connection.readyState).toEqual(1);
+    const conn = await mongooseInit();
+    expect(conn.connection.readyState).toEqual(1);
   });
 });
